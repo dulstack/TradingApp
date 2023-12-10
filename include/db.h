7a7668a52ddb533a0c9e093bcccedd2d5280a120
dulstack <doowew100@gmail.com>
2023-12-10 11:08:49 +0200

    modified:   CMakeLists.txt 	modified:   include/db.h 	modified:   src/CMakeLists.txt 	modified:   src/db.cpp

--- include/db.h
+++ include/db.h
@@ -12,11 +12,21 @@ class DB{
   bool open(const char*);
   void close();
   bool create_account(const std::string& user, const std::string& password);
+  bool verify_login(int id,  const std::string& password, bool is_hash=false);
   bool verify_login(const std::string& user, const std::string& password, bool is_hash=false);
-  void logout();
   void list_curr();
-  bool bal();
+  bool bal();				//print the balance
+  int get_user_id(const std::string& name);
+  float get_user_bal(int id);
+  int get_currency_ID(const std::string& currency);		//not implemented yet
+  float get_price(const std::string& currency);
+  bool set_price(const std::string& currency, float price);
+  bool buy(const std::string& currency, float quanity);		//will increase currency price by 0.1*quanity
+  bool sell(const std::string& currency, float quanity); 		//will decrease currency price by 0.1*quanity
+  void logout();
  private:
+  //TODO: use user ID instead of username
+  int UID;
   std::string* username;
   std::string* hash;
   sqlite3* db;
--- src/db.cpp
+++ src/db.cpp
@@ -4,14 +4,21 @@ static std::string encrypt(const std::string& arg){
  std::string res=arg;
  return res;
 }
-bool DB::verify_login(const std::string& user, const std::string& password, bool is_hash){
- //TODO: replace \ with \\ and ' with \' to prevent from SQL injections
- if(user.size()<1||password.size()<1)return 0;
+int DB::get_user_id(const std::string& name){
+ int res=-1;
+ std::string sql=std::string("SELECT ID FROM users where name='")+name+"';";
+ if(!sqlite3_prepare(db, sql.c_str(), -1, &query,NULL)){
+  if(sqlite3_step(query)!=SQLITE_DONE)res=sqlite3_column_int(query,0);
+ }
+ sqlite3_finalize(query);query=NULL;
+ return res;
+}
+bool DB::verify_login(int id, const std::string& password, bool is_hash){
  bool success=0;
  std::string hash;
  if(!is_hash){hash=encrypt(password);}
  else{hash=password;} //2nd argument is already encrypted
- std::string sql=std::string("SELECT name, hash FROM users where name='")+user+std::string("'and hash='")+hash+"';";
+ std::string sql=std::string("SELECT name, hash FROM users where ID='")+std::to_string(id)+std::string("'and hash='")+hash+"';";
  if(sqlite3_prepare(db, sql.c_str(), -1, &query,0)){
   #ifdef DEBUG
   fprintf(stderr,"Query error\n%s\n", sql.c_str());
@@ -23,6 +30,15 @@ bool DB::verify_login(const std::string& user, const std::string& password, bool
  sqlite3_finalize(query);
  query=NULL;
  return success;
+}
+bool DB::verify_login(const std::string& user, const std::string& password, bool is_hash){
+ //TODO: replace \ with \\ and ' with \' to prevent from SQL injections
+ if(user.size()<1||password.size()<1)return 0;
+ int id;
+ if((id=this->get_user_id(user))==-1){
+  return 0;
+ }
+ return this->verify_login(id, password, is_hash);
 }
 
 bool DB::create_account(const std::string& user, const std::string& password){
@@ -40,6 +56,7 @@ bool DB::create_account(const std::string& user, const std::string& password){
  return 1;
 }
 DB::DB(){
+ UID=-1;
  db=NULL;
  query=NULL;
  username=NULL;
@@ -94,8 +111,30 @@ void DB::save_credentials(const std::string& name, const std::string& hash){
  this->hash=new std::string(hash);
  this->logged_in=1;
 }
+float DB::get_user_bal(int id){
+ //TODO make verify_login use id
+ if(!this->logged_in||!this->verify_login(*username, *hash, true)){
+  fprintf(stderr, "Error: you are not logged in\n");
+  return -1;
+ }
+ std::string sql="SELECT balance FROM users where ID='"+std::to_string(id)+"';";
+ float res=-1;
+ if(sqlite3_prepare(db, sql.c_str(),-1, &query, 0)){
+  #ifdef DEBUG
+  fprintf(stderr, "Query error in DB::bal()\nQuery: %s\n", sql.c_str());
+  #endif
+ }
+ else if(sqlite3_step(query)==SQLITE_DONE){fprintf(stderr, "Something went wrong\n");}
+ else{
+  res=(float)sqlite3_column_double(query, 0);
+ }
+ sqlite3_finalize(query);
+ query=NULL;
+ return res;
 
+}
 bool DB::bal(){
+ //TODO: modify some stuff here to use get_user_bal
  if(!this->logged_in||!this->verify_login(*username, *hash, true)){
   fprintf(stderr, "Error: you are not logged in\n");
   return 0;
@@ -121,5 +160,53 @@ void DB::logout(){
  this->logged_in=0;
  if(username){delete username;username=NULL;}
  if(hash){delete hash;hash=NULL;}
+}
+
+float DB::get_price(const std::string& currency){
+ float res=-1;
+ std::string sql=std::string("SELECT price FROM currencies where name='"+currency+"';");
+ if(!sqlite3_prepare(db, sql.c_str(), -1, &query,NULL)){
+  if(sqlite3_step(query)!=SQLITE_DONE){
+   res=(float)sqlite3_column_double(query, 0);
+  }
+  else{
+   fprintf(stderr, "Currency \"%s\"doesn't exist\n", currency.c_str());
+  }
+ }
+ #ifdef DEBUG
+ else{
+  fprintf(stderr, "Query error in DB::get_price\nSQL query: %s\n", sql.c_str());
+ }
+ #endif
+ sqlite3_finalize(query);query=NULL;
+ return res;
+}
+
+bool DB::set_price(const std::string& currency, float price){
+ bool success=0;
+ std::string sql=std::string("UPDATE currencies SET price='")+std::to_string(price)+std::string("' WHERE name='")+currency+"';";
+ if(!sqlite3_exec(db, sql.c_str(), 0,0,0)){
+  success=1;
+ }
+ #ifdef DEBUG
+ else{
+  fprintf(stderr, "Query error in DB::set_price\nSQL query: %s\n", sql.c_str());
+ }
+ #endif
+ return success;
+}
+bool DB::buy(const std::string& currency, float quanity){
+ //TODO: add login verification(ID should be used)
+ bool success=0;
+ float total=0, price=get_price(currency); //total means money that will be spent
+ if(price==-1){return 0;}
+ if(quanity<=0){fprintf(stderr, "Error: invalid quanity\n");return 0;}
+ total=price*quanity;
+ 
+ return success;
+}
+bool DB::sell(const std::string& currency, float quanity){
+ bool success=0;
+ return success;
 }
 
